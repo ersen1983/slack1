61ee371a25d64d23ef56b026e9403209522e39ea
ersen ozcan <ersen2008@hotmail.com>
2020-05-08 15:45:43 -0400

    add LogCall.java

--- src/day_1/ozcanHouse.java
+++ src/day_1/ozcanHouse.java
@@ -3,5 +3,6 @@ package day_1;
 public class ozcanHouse {
     public static void main(String[] args) {
         System.out.println("here is ozcan house Ersen Sevda Ekrem Ceyda");
+        System.out.println("here is ozcan house Ersen Sevda Ekrem Ceyda");
     }
 }
