b9a5bd528197789040962a685ab0fd71f2e0c706
ersen ozcan <ersen2008@hotmail.com>
2020-05-08 15:48:03 -0400

    add LocalCall.java

--- src/day_1/ozcanHouse.java
+++ src/day_1/ozcanHouse.java
@@ -4,5 +4,6 @@ public class ozcanHouse {
     public static void main(String[] args) {
         System.out.println("here is ozcan house Ersen Sevda Ekrem Ceyda");
         System.out.println("here is ozcan house Ersen Sevda Ekrem Ceyda");
+        System.out.println("here is ozcan house Ersen Sevda Ekrem Ceyda");
     }
 }
