15697f20ac843cd8399aaf5fa501b2b52641e239
ersen1983 <64223825+ersen1983@users.noreply.github.com>
2020-05-08 15:41:08 -0400

    Update ozcanHouse.java

--- src/day_1/ozcanHouse.java
+++ src/day_1/ozcanHouse.java
@@ -2,6 +2,6 @@ package day_1;
 
 public class ozcanHouse {
     public static void main(String[] args) {
-        System.out.println("here is ozcan house");
+        System.out.println("here is ozcan house Ersen Sevda Ekrem Ceyda");
     }
 }
